af4beb23821d8f526ece039d720cd2f71f616fdc
Tyaa <tyaa@Rian-Ubays-MacBook-Air.local>
2018-07-19 17:45:37 +0700

    INPUT PRODUCT SUCCESSFULLY

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -14,6 +16,12 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct() {
+        $this->middleware('auth:api')->except('index','show');
+        // return $request->all();
+    }
+
     public function index()
     {
         // return ProductResource::collection(Product::all());
@@ -37,9 +45,23 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        // return 'abcd';
+        $product = new Product;
+        $product->prod_name = $request->prod_name;
+        $product->prod_desc = $request->prod_desc;
+        $product->prod_price = $request->prod_price;
+        $product->prod_stock = $request->prod_stock;
+        $product->prod_discount = $request->prod_discount;
+        $product->prod_type = $request->prod_type;
+
+        $product->save();
+
+        // return $request->all();
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
